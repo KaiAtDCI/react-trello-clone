78d8c738f1cbc4d8693fcb422e3ef3cc1971df8c
KaiAtDCI <kaiatdci@web.de>
2021-07-21 15:32:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Simple-Trello-Copy
+
+- You will need to figure out the task by looking at the README and the existing files
+- Track your hours when working for this one, add your tracking info here into the README.
+- Add milestones of your own choosing as tracking points, for example
+
+```
+    1h 30m: able to render tasks
+    2h 00m: able to delete tasks
+    4h 30m: able to add new task using prompt()
+    5h 30m: able to add new task list using prompt()
+```
+
+- Spend a maximum of 5 hours on this; see how far you can get it!
+- Try to implement as many features as you can!
+- **While working fast, still write elegant code that is formatted well!**
+
+**trello-copy-exercise**
+*  Work on rendering a single list of cards. For more practice, render multiple lists of cards side-by-side.
+ <img src="./src/images/trello-copy.png" alt="trello card item" width="500" />
---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import Project from "./components/Project";
+import {projects} from "./data";
+import './App.scss'
+
+export default class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			projects: projects,
+		};
+	}
+
+	addTask = (id, description) => {
+		const newTask = {
+			id: Math.round(Math.random() * 10000000),
+			text: description,
+		}
+		const projects = [...this.state.projects];
+		projects.find(project => project.id === id).tasks.push(newTask);
+		this.setState({projects: projects})
+	}
+
+	deleteTask = (projectId, taskId) => {
+		console.log('delete: ');
+		const projects = [...this.state.projects];
+		const tasks = projects.find(project => project.id === projectId).tasks;
+		projects.find(project => project.id === projectId).tasks = tasks.filter(task => task.id !== taskId);
+		this.setState({ projects: projects }, () => console.log(this.state.projects))
+	}
+
+	addProject = () => {
+		const newProject = {
+			id: Math.round(Math.random() * 1000000),
+			title: '',
+			tasks: [],
+		}
+		const projects = [...this.state.projects];
+		projects.push(newProject);
+		this.setState({projects: projects});
+	}
+
+	deleteProject = (id) => {
+		const projects = this.state.projects.filter(project => project.id !== id);
+		// console.log(projects);
+		this.setState({ projects: projects });
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<input type='button'
+					   value='Add Project'
+					   className='btn btn-primary'
+					   onClick={this.addProject}/>
+				<div className='projects'>
+					{this.state.projects.map((project) =>
+						<Project key={project.id}
+								 project={project}
+								 deleteProject={this.deleteProject}
+								 addTask={this.addTask}
+								 deleteTask={this.deleteTask}
+						/>)}
+				</div>
+			</div>
+		)
+	}
+}
